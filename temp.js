1633986cafa805869619e927fcd060b4170e6c10
CAOXU <2804585060@qq.com>
2024-02-01 14:36:55 +0800

    temp.js

--- temp.js
+++ temp.js
@@ -1,10 +1,11 @@
 // ==UserScript==
-// @name         New Userscript
+// @name         Statement scripts
 // @namespace    http://tampermonkey.net/
 // @version      2024-02-01
-// @description  try to take over the world!
-// @author       You
+// @description  Automate click-and-click statement scripts
+// @author       women
 // @match        https://greasyfork.org/zh-CN/users/1255849-womenhao
+// @match        *://*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=greasyfork.org
 // @grant        none
 // ==/UserScript==
