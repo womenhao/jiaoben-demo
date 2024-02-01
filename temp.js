5cc19f2536544da40a2cc3199b546108d0967f30
CAOXU <2804585060@qq.com>
2024-02-01 14:08:32 +0800

    main

---
+++ temp.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      2024-02-01
+// @description  try to take over the world!
+// @author       You
+// @match        https://greasyfork.org/zh-CN/users/1255849-womenhao
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=greasyfork.org
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    
+})();
